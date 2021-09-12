444bdc0d2c9d3bdbf61cdf33131630d0f682fb24
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-09-12 19:07:39 +0200

    Add MaritimeBoundaries project

---
+++ src/SeabatsData.MaritimeBoundaries/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SeabatsData.MaritimeBoundaries
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var geoJson12nm = await File.ReadAllTextAsync(@"U:\seabats\maritime-boundaries\eez_12nm_v3.geojson");
+            var bounds12nm = JsonConvert.DeserializeObject(geoJson12nm);
+
+            var geoJson24nm = await File.ReadAllTextAsync(@"U:\seabats\maritime-boundaries\eez_24nm_v3.geojson");
+            var bounds24nm = JsonConvert.DeserializeObject(geoJson24nm);
+        }
+    }
+}
