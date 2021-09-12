b63bf9a12f564345ae0afbc6a4697ad2a7dec115
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2021-09-13 01:02:05 +0200

    Add SeabatsData.Core project

--- src/SeabatsData.App/Program.cs
+++ src/SeabatsData.App/Program.cs
@@ -4,49 +4,32 @@ using System.IO;
 using System.IO.Compression;
 using System.Threading.Tasks;
 using GeoJSON.Net.Geometry;
-using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
+using SeabatsData.Core;
 
 namespace SeabatsData.App
 {
-    class Program
+    class Program : ProgramBase
     {
         public static async Task Main(string[] args)
         {
-            var configuration = new ConfigurationBuilder()
-                .AddEnvironmentVariables()
-                .Build();
-
-            WriteHeading("Configuration");
-
-            var inputDir = configuration["SeabatsData:InputDir"];
-            var outputDir = configuration["SeabatsData:OutputDir"];
-            var icaos = configuration["SeabatsData:ICAOs"];
-
-            Console.WriteLine($"Input Directory:  {inputDir}");
-            Console.WriteLine($"Output Directory: {outputDir}");
-            Console.WriteLine($"ICAOs:            {icaos}");
-
-            if (!Directory.Exists(outputDir))
-                Directory.CreateDirectory(outputDir);
-
             WriteHeading("Files");
 
-            var files = Directory.GetFiles(inputDir, "*.*", SearchOption.AllDirectories);
-            var gzFiles = Directory.GetFiles(inputDir, "*.gz", SearchOption.AllDirectories);
-            var jsonGzFiles = Directory.GetFiles(inputDir, "*.json.gz", SearchOption.AllDirectories);
-            var csvGzFiles = Directory.GetFiles(inputDir, "*.csv.gz", SearchOption.AllDirectories);
+            var files = Directory.GetFiles(InputDir, "*.*", SearchOption.AllDirectories);
+            var gzFiles = Directory.GetFiles(InputDir, "*.gz", SearchOption.AllDirectories);
+            var jsonGzFiles = Directory.GetFiles(InputDir, "*.json.gz", SearchOption.AllDirectories);
+            var csvGzFiles = Directory.GetFiles(InputDir, "*.csv.gz", SearchOption.AllDirectories);
 
             Console.WriteLine($"Files:           {files.Length}");
             Console.WriteLine($"*.gz Files:      {gzFiles.Length}");
             Console.WriteLine($"*.json.gz Files: {jsonGzFiles.Length}");
             Console.WriteLine($"*.csv.gz Files:  {csvGzFiles.Length}");
             
-            foreach (var icao in icaos.Split(","))
+            foreach (var icao in Icaos.Split(","))
             {
                 WriteHeading($"Exporting traces of ICAO: #{icao}...");
 
-                var icaoFiles = Directory.GetFiles(inputDir, $"*{icao}*", SearchOption.AllDirectories);
+                var icaoFiles = Directory.GetFiles(InputDir, $"*{icao}*", SearchOption.AllDirectories);
 
                 var flights = new List<GeoJSON.Flight>();
 
@@ -69,37 +52,15 @@ namespace SeabatsData.App
                         {
                             Icao = adsbExchangeFlight.Icao,
                             Duration = adsbExchangeFlight.DurationSeconds,
-                            Label = GetLabel(adsbExchangeFlight.Icao)
+                            Label = GetAircraftNameByIcao(adsbExchangeFlight.Icao)
                         });
 
                     flights.Add(geoJson);
                 }
 
                 var serialized = JsonConvert.SerializeObject(flights);
-                await File.WriteAllTextAsync(Path.Combine(outputDir, $"{icao}.json"), serialized);
+                await File.WriteAllTextAsync(Path.Combine(OutputDir, $"{icao}.json"), serialized);
             }
-        }
-
-        private static string GetLabel(string icao)
-        {
-            // TODO move to where it belongs
-            icao = icao.ToUpper();
-            return icao switch
-            {
-                "406658" => "G-DMPP",
-                "407637" => "G-WKTH",
-                "40785B" => "G-WKTI",
-                "43ED4B" => "2-WKTJ",
-                "4D232D" => "HRON",
-                _ => throw new InvalidOperationException($"Unknown ICAO: #{icao}")
-            };
-        }
-
-        private static void WriteHeading(string heading)
-        {
-            Console.WriteLine();
-            Console.WriteLine(heading.ToUpper());
-            Console.WriteLine();
         }
     }
 }
---
+++ src/SeabatsData.Core/ProgramBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace SeabatsData.Core
+{
+    public abstract class ProgramBase
+    {
+        static ProgramBase()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+
+            WriteHeading("Configuration");
+
+            InputDir = configuration["SeabatsData:InputDir"];
+            OutputDir = configuration["SeabatsData:OutputDir"];
+            Icaos = configuration["SeabatsData:ICAOs"];
+
+            Console.WriteLine($"Input Directory:  {InputDir}");
+            Console.WriteLine($"Output Directory: {OutputDir}");
+            Console.WriteLine($"ICAOs:            {Icaos}");
+
+            if (!Directory.Exists(OutputDir))
+                Directory.CreateDirectory(OutputDir);
+        }
+
+        protected static string InputDir { get; }
+        protected static string OutputDir { get; }
+        protected static string Icaos { get; }
+
+        protected static void WriteHeading(string heading)
+        {
+            Console.WriteLine();
+            Console.WriteLine(heading.ToUpper());
+            Console.WriteLine();
+        }
+
+        protected static string GetAircraftNameByIcao(string icao)
+        {
+            // TODO move to where it belongs
+            icao = icao.ToUpper();
+            return icao switch
+            {
+                "406658" => "G-DMPP",
+                "407637" => "G-WKTH",
+                "40785B" => "G-WKTI",
+                "43ED4B" => "2-WKTJ",
+                "4D232D" => "HRON",
+                _ => throw new InvalidOperationException($"Unknown ICAO: #{icao}")
+            };
+        }
+    }
+}
